ce2d30f8fe170b0f406dee8349977dacd2129a9d
Oleksii-Davydov <davydovalekcey1992@gmail.com>
2023-02-13 16:02:51 +0200

    Update index.js
    v2

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-const xml = new XMLHttpRequest();
-const xml2 = new XMLHttpRequest();
+// const xml = new XMLHttpRequest();
+// const xml2 = new XMLHttpRequest();
+//
+// xml.open("Get","request/data.json", false);
+// xml2.open("Get","request/data2.json", false);
+//
+// xml.send();
+// xml2.send();
+//
+// let parse = response => JSON.parse(response);
+// console.log([...parse(xml.responseText).children, ...parse(xml2.responseText).children])
 
-xml.open("Get","request/data.json", false);
-xml2.open("Get","request/data2.json", false);
+function requestData(method, action, callback) {
+    const xml = new XMLHttpRequest();
+    xml.open(method, action);
+    xml.send();
+    let parse = response => JSON.parse(response);
+    xml.addEventListener("readystatechange", () => {
+        if(xml.readyState === 4 && xml.status === 200) {
+            const response = parse(xml.responseText).children;
 
-xml.send();
-xml2.send()
-
-let parse = response => JSON.parse(response);
-console.log([...parse(xml.responseText).children, ...parse(xml2.responseText).children])
-
-
+            callback(response);
+        }
+    })
+}
+function renderElem(response) {
+    response.forEach(item => console.log(item));
+}
+requestData("GET", "request/data.json", renderElem);
+requestData("GET", "request/data2.json", renderElem);
